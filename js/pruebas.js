0268d8a2948e20570b31c8ac5c8a2ed4b9c66759
Kofe-efoK <leandrotomasjofre@gmail.com>
2022-06-21 19:21:05 -0300

    Clase 7
    Actividades realizadas durante la visualización de la clase 7

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,20 @@
-function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
-
-  console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
+/*
+function sumar(a, b) {
+    return a + b;
 }
+console.assert(sumar(1,2) === 3, "La suma de 1 y 2 no dio 3");
+*/
 
+function probarValidarNombre() {
+    console.assert(
+        validarNombre("") === "Este campo debe tener al menos 1 caracter", 
+        "Validar nombre no validó que un string no sea vacío"
+    );
+    
+    console.assert(
+        validarNombre("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") === 
+        "Este campo debe tener menos de 50 caracteres", 
+        "Validar nombre no validó que un string no tenga más de 50 carácteres"
+    );
+}
 probarValidarNombre();
